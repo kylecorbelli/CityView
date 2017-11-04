4c19925e8f425e8d1c0ed3809b44723b48b9ede3
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-11-04 16:36:15 -0700

    Add react-native-navigation

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
+import { Navigation } from 'react-native-navigation'
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('CityView', () => App);
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const MapSearchScreen = () => (
+  <View style={{
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
+  }}>
+    <Text>Map Search Screen</Text>
+  </View>
+)
+
+MapSearchScreen.navigatorStyle = {
+  navBarHidden: true,
+}
+
+const ListSearchScreen = () => (
+  <View style={{
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
+  }}>
+    <Text>List Search Screen</Text>
+  </View>
+)
+
+Navigation.registerComponent('MapSearchScreen', () => MapSearchScreen)
+Navigation.registerComponent('ListSearchScreen', () => ListSearchScreen)
+
+Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: 'Map',
+      screen: 'MapSearchScreen',
+      title: 'Map',
+    },
+    {
+      label: 'List',
+      screen: 'ListSearchScreen',
+      title: 'List',
+    },
+  ],
+})
+
+// AppRegistry.registerComponent('CityView', () => App);
